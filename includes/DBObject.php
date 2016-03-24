8274d07ad48b73bf175f6392977aa1cb3ba57af7
quo123 <cs1.6.123@gmail.com>
2016-03-24 22:33:05 +0800

    updated DBObject to v0.5.1 now supports named parameters

--- includes/DBObject.php
+++ includes/DBObject.php
@@ -1,11 +1,12 @@
 <?php
 /**
- *  DB Object class made to simplify query code
+ *  DB Object wrapper class made to simplify query code
  *  Copyright 2014-2016 Alvin R. Betoya
  *  Released under the MIT license (https://tldrlegal.com/license/mit-license)
- *	v0.5.0
+ *	v0.5.1
  * 
  *	changelog:
+ *	0.5.1	- allowed named binding
  *	0.5.0	- added support for prepared statements
  *	0.4.4	- added variable timezone
  *	0.4.3	- initialized error
@@ -56,6 +57,9 @@
  *		} else {
  *			//TODO: code for failed update
  *		}
+ * 
+ *	Prepared statements:
+ *	
  */
 class DBObject {
 	private $database;
@@ -75,20 +79,33 @@ class DBObject {
 	private $timezone = 'Asia/Manila'; // added v0.4.4
 
 	/**
-	 * Create a new DB connection using either of the following:
-	 * $db = new DBObject("database_name");
-	 * $db = new DBObject("database_name", "host_name");
-	 * $db = new DBObject("database_name", "host_name", "user_name");
-	 * $db = new DBObject("database_name", "host_name", "user_name", "password");
+	 * Added v0.5.1 
+	 * @var mysqli_stmt 
+	 */
+	private $namedstmt;
+	private $paramnames;
+	private $paramtypes;
+	private $paramvals;
+	
+	
+	/**
+	 * Create a new DB connection
+	 * @param string $_database The database name.
+	 * @param string $_host The host name/url.
+	 * @param string $_dbuser The database user name.
+	 * @param string $_dbpass The database password.
 	 */
 	public function __construct($_database, $_host = 'localhost', $_dbuser = 'root', $_dbpass = '') {
 		$this->host = $_host;
 		$this->dbuser = $_dbuser;
 		$this->dbpass = $_dbpass;
-		$this->database = $_database;		
+		$this->database = $_database;
 	}
 	
-	/** Escapes using mysqli_real_escape_string. */
+	/**
+	 * Escapes using mysqli_real_escape_string.
+	 * @param string $var The string to escape
+	 */
 	public function escape($var) {
 		$this->connect();
 		mysqli_set_charset($this->con, 'utf8'); //added v0.3.1
@@ -98,10 +115,10 @@ class DBObject {
 	}
 	
 	/**
-	 * Simple query function. Logs the following:
-	 * last insert id
-	 * error(s)
-	 * number of affected rows
+	 * Simple query function. Stores the following: <br />
+	 * last insert id<br />
+	 * error(s)<br />
+	 * number of affected and retrieved rows<br />
 	 * @param string $sql SQL string
 	 * @return mysqli_result Query result set
 	 */
@@ -122,9 +139,9 @@ class DBObject {
 	}
 	
 	/**
-	 * added v0.5.0
-	 * Prepare a statement.
-	 * @param string $sql SQL string
+	 * Added v0.5.0
+	 * Prepare a statement. To use named parameters, use <code>namedPrepare()</code> instead.
+	 * @param string $sql The SQL string.
 	 * @return mysqli_stmt The prepared statement
 	 */
 	public function prepare($sql) {
@@ -136,12 +153,14 @@ class DBObject {
 	}
 	
 	/**
-	 * added v0.5.0
-	 * Executes a prepared statement.
+	 * Added v0.5.0
+	 * Executes a prepared statement. When using named parameters, use <code>executeQuery()</code> instead.
 	 * @param mysqli_stmt $query The query to execute
+	 * @param boolean $close Closes the query if set to <code>TRUE</code>.
+	 * Set to <code>FALSE</code> to allow further parameter binds on this prepared statement.
 	 * @return mysqli_result Query result set
 	 */
-	public function execute(mysqli_stmt $query) {
+	public function execute(mysqli_stmt $query, $close = true) {
 		$query->execute();
 		$result = $query->get_result();
 		$this->lastid = $query->insert_id;
@@ -149,10 +168,86 @@ class DBObject {
 		$this->errno = $query->errno;
 		$this->affected = $query->affected_rows;
 		$this->numrows = $query->num_rows;
-		$query->close();
-		mysqli_close($this->con);
-		
+		if ($close) {
+			$query->close();
+			mysqli_close($this->con);
+		}
+//		error_log('result: ' . print_r($this, true));
 		return $result;
+	}
+	
+	/**
+	 * Added v0.5.1
+	 * Prepare a statement with named parameters.
+	 * @param string $sql The sql to prepare. Named parameters are supported via the following format:
+	 * <pre><code>?<PARAMETER_NAME>:<PARAMETER_TYPE></code></pre>
+	 * e.g. <code>SELECT * FROM example WHERE username = ?username:s</code><br /><br />
+	 * <code>PARAMETER_TYPE</code> can be any of the following:<br />
+	 * s -> string<br />
+	 * i -> integer<br />
+	 * d -> decimal<br />
+	 * b -> blob<br />
+	 */
+	public function namedPrepare($sql) {
+		$this->clearParams();
+		$pattern = "/\?([A-Za-z_]+[\w]*):([idsb])/";
+		preg_match_all($pattern, $sql, $matches, PREG_SET_ORDER);
+		foreach ($matches as $val) {
+			error_log($val[1]);
+			$this->paramnames[] = $val[1];
+			$this->paramtypes[] = $val[2];
+		}
+		$sql = preg_replace($pattern, '?', $sql);
+		error_log('sql: ' . $sql);
+		error_log('paramnames: ' . print_r($this->paramnames, true));
+		error_log('paramtypes: ' . print_r($this->paramtypes, true));
+
+		try {
+			$this->namedstmt = $this->prepare($sql);
+			error_log('namedstmt: ' . print_r($this->namedstmt, true));
+			return true;
+		} catch (Exception $ex) {
+			return false;
+		}
+	}
+	
+	/**
+	 * Added v0.5.1
+	 * Bind named parameters to prepared statement.
+	 * @param array $params An associative array of parameters to bind.
+	 * Keys should coincide with the names of the parameters.<br />
+	 * This just stores the parameters in the <code>DBObject</code> object.
+	 * Actual binding happens only when the query is executed.
+	 */
+	public function namedBind($params) {
+		foreach ($params as $key => $val) {
+			$this->paramvals[$key] = $val;
+		}
+		error_log('paramvals: ' . print_r($this->paramvals, true));
+	}
+	
+	/**
+	 * Added v0.5.1
+	 * Execute prepared statement with named parameters.
+	 * @param boolean $close Closes the query if set to <code>TRUE</code>.
+	 * Set to <code>FALSE</code> to allow further parameter binds on this prepared statement.
+	 * @return mysqli_result Query result set.
+	 */
+	public function executeQuery($close = true) {
+		$vals = array();
+		$types = array(join('', $this->paramtypes));
+		foreach ($this->paramnames as $name) {
+			$vals[] = &$this->paramvals[$name];
+		}
+		
+		$bindarray = array_merge($types, $vals);
+		error_log('bindarray: ' . print_r($bindarray, true));
+		$ref = new ReflectionClass('mysqli_stmt');
+		$method = $ref->getMethod('bind_param');
+		$method->invokeArgs($this->namedstmt, $bindarray);
+//		call_user_func_array('mysqli_stmt_bind_param', $bindarray);
+		error_log('namedstmt: ' . print_r($this->namedstmt, true));
+		return $this->execute($this->namedstmt, $close);
 	}
 	
 	private function connect() {
@@ -164,6 +259,12 @@ class DBObject {
 		return true;
 	}
 
+	private function clearParams() {
+		$this->paramnames = array();
+		$this->paramtypes = array();
+		$this->paramvals = array();
+	}
+	
 	public function setDatabase($_database) {
 		$this->database = $_database;
 	}
